704cf8aa1b2813d00618e9a7977ca673a6217526
reddymanyam <reddappareddy18@gmail.com>
2025-02-26 00:47:11 +0530

    update: app.js is updated

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {                      // these are the routes.....!
-    res.send("hello hello hello my dear friends....!")
+app.get("/user", (req, res) => {                      
+    res.send({"firstname":"reddy", "secondname":"manyam"})
 });
 
-app.use("/test", (req, res) => {                       // these are the routes.........!
+app.post("/user", (req, res) => {                      
     res.send("hello i am from test server...!")
 });
 
 app.use((req, res) => {                            //we want to keep the genral middleware at the end or else it will catch the all the routes
-    res.send("hello i am server...!")
-});
+    res.send("hello i am server...!")              //we dont want to use, app.use("/") for routing, because it will catch all the routes like get,put,post 
+});                                                //To overcome this we want to use app.get("/user"), app.post("/user")
 
 app.listen(7777, () => {                           //creating the port (port is nothing but a server...!)
     console.log("Hello from the server......!")
