7294f1a7cddac568b1128981d48c2f843556a56c
reddymanyam <reddappareddy18@gmail.com>
2025-02-27 23:51:41 +0530

    update: all the routing techniques are updated

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ app.post("/user", (req, res) => {
     res.send("hello i am from test server...!")
 });
 
+app.post("/user", (req, res) =>{
+    res.send({"firstName" : "Reddy", "lastName":"Reddy"})
+});
+
+app.put("/user", (req,res)=>{
+    res.send({"firstName" : "Reddy", "lastName":"Roy"})
+});
+
+app.delete("/user", (req,res) =>{
+    console.log("succesfully deleted the user data...!");
+});
+
 app.use((req, res) => {                            //we want to keep the genral middleware at the end or else it will catch the all the routes
     res.send("hello i am server...!")              //we dont want to use, app.use("/") for routing, because it will catch all the routes like get,put,post 
 });                                                //To overcome this we want to use app.get("/user"), app.post("/user")
