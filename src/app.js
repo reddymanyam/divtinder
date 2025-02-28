b347b3b0571a300a051417bb4f225b0fd8633b87
reddymanyam <reddappareddy18@gmail.com>
2025-02-28 23:17:54 +0530

    updated:next req updated

--- src/app.js
+++ src/app.js
@@ -2,25 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {                      
-    res.send({"firstname":"reddy", "secondname":"manyam"})
-});
+// app.get("/user", (req, res) => {                      
+//     res.send({"firstname":"reddy", "secondname":"manyam"})
+// });
 
-app.post("/user", (req, res) => {                      
-    res.send("hello i am from test server...!")
-});
+app.get('/user', (req, res, next) => {
+    console.log("this is user response...!");
+    res.send("response 1");
+    next();
+},
+    (req, res) => {
+        res.send("response 2") 
+    }
+);
 
-app.post("/user", (req, res) =>{
-    res.send({"firstName" : "Reddy", "lastName":"Reddy"})
-});
 
-app.put("/user", (req,res)=>{
-    res.send({"firstName" : "Reddy", "lastName":"Roy"})
-});
+// app.post("/user", (req, res) => {                      
+//     res.send("hello i am from test server...!")
+// });
 
-app.delete("/user", (req,res) =>{
-    console.log("succesfully deleted the user data...!");
-});
+// app.post("/user", (req, res) =>{
+//     res.send({ firstName : "Reddy", lastName:"Reddy"})
+// });
+
+// app.put("/user", (req,res)=>{
+//     res.send({firstName : "Reddy", lastName:"Roy"})
+// });
+
+// app.delete("/user", (req,res) =>{
+//     console.log("succesfully deleted the user data...!");
+// });
 
 app.use((req, res) => {                            //we want to keep the genral middleware at the end or else it will catch the all the routes
     res.send("hello i am server...!")              //we dont want to use, app.use("/") for routing, because it will catch all the routes like get,put,post 
