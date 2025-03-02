9f878df51e43641aa90403b039db04468aea7687
reddymanyam <reddappareddy18@gmail.com>
2025-03-02 23:02:21 +0530

    updated: middleware updated

---
+++ src/middlewares/auth.js
@@ -0,0 +1,12 @@
+const audminAuth = (req, res, next) => {
+    const token = "xyz";
+    const isAuthenticated = token === "xyz";
+
+    if (!isAuthenticated) {
+        res.status(402).send("authentication failed");
+    } else {
+        next();
+    }
+}
+
+module.exports= {audminAuth};
