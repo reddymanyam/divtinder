ff718d7701de35cdf7300e35a6867b7433427b8a
reddymanyam <reddappareddy18@gmail.com>
2025-03-16 23:20:33 +0530

    datbase is updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://reddappareddy18:fX7qq3Rh8j7hPF4U@devtinder.pmdxg.mongodb.net/?retryWrites=true&w=majority&appName=Devtinder");
+    await mongoose.connect("mongodb+srv://reddappareddy18:fX7qq3Rh8j7hPF4U@devtinder.pmdxg.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Devtinder");
 };
 
 
