1f098fc1087fd628e3a9fe1a5a4035bef3ba53bd
reddymanyam <reddappareddy18@gmail.com>
2025-03-20 17:46:43 +0530

    updated: apis are updated

--- src/app.js
+++ src/app.js
@@ -5,78 +5,58 @@ const User = require('./model/user');
 
 app.use(express.json());
 
-// app.post('/signup', async (req, res) => {
-
-
-//     const user = new User(req.body);
-//     console.log(req.body);
-
-//     try {
-//         await user.save();
-//         res.send("data saved succesfully")
-//     }
-//     catch (err) {
-//         console.log("Data is not sent successfully");
-//     }
-// })
-
-app.post('/signup', async (req, res) => {
+app.get('/users', async (req, res) => {
     try {
-        const user = new User(req.body);
-        await user.save(); // Use await to ensure it completes
-        console.log("Data saved successfully:", user);
-        res.status(201).send("Data saved successfully");
+        const users = await User.find({});  // Get the all the data from the database
+        res.status(200).json({ "message": "Data is Successfully send", users })
     } catch (err) {
-        console.error("Data is not sent successfully:", err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).json({ message: "something went wrong", error: err.message });  // This is very Important for knowing what type of error , it is throwing
     }
 });
 
-// Getting the data of the one person based on the email id
-app.get('/user', async (req, res) => {
+app.post('/users', async (req, res) => {
 
-    const emailId = req.body.email;
     try {
-        const users = await User.findOneAndDelete({ email: emailId });
-        if (users.length === 0) {
-            res.status(400).send("user not found...!");
-        } else {
-            res.status(200).send(users);
-        }
-    }
-    catch (err) {
-        res.status(401).send("something went wrong")
-    }
-})
-
-// Getting the data of the person by the Id(mongodb genrated id)
-app.get('/users', async (req, res) => {
-
-    const mongoId = req.body._id;
-    try {
-        const users = await User.findById({ _id: mongoId });
-        if (users.length === 0) {
-            res.status(400).send("user is not found");
-        } else {
-            res.status(200).send(users)
-        }
+        const users = new User(req.body);
+        await users.save();
+        res.status(200).json(users);
     } catch (err) {
-        res.status(400).send("Something went Wrong...!");
+        res.status(500).json({ message: "something went wrong", error: err.message });
     }
-})
+}
+);
 
-// Getting all the data from the database
-app.get('/feed', async (req, res) => {
+
+app.patch('/users/:id', async (req, res) => {
+
+    const reqId = req.params.id;
+    const updatedData = req.body;
+
     try {
-        const users = await User.find({});
-        if (!users || users.length === 0) {
-            return res.status(404).send("No users found");
+        const users = await User.findOneAndUpdate({ _id: reqId }, updatedData, {
+            returnDocument: "after",
+            returnValidators: true,
+        });
+        if (!users) {
+            res.status(404).json({ message: "users is not found" });
         }
-        res.status(200).send(users);
+        res.status(200).json({ message: "users Data Updated Succesfully", users });
+    } catch (err) {
+        res.status(500).json({ message: "something went wrong", error: err.message });
     }
-    catch (err) {
-        console.error("Error fetching users:", err);
-        res.status(500).send("Something Went Wrong...!");
+});
+
+app.delete('/users/:id', async (req, res) => {
+
+    const reqId = req.params.id;
+    try {
+        const users = await User.findByIdAndDelete({ _id: reqId });
+        if (!users) {
+            res.status(404).json({ message: "users is not found" });
+        }
+        res.status(200).json({ message: "data deleted succesfully", users });
+    } catch (err) {
+        res.status(500).json({ message: "something went wrong", error: err.message });
     }
 })
 
