c72dbaa662b7724329a3b7c31b7c4b71df9dd1bf
reddymanyam <reddappareddy18@gmail.com>
2025-03-20 17:38:38 +0530

    updated: Schema Validations

--- src/model/user.js
+++ src/model/user.js
@@ -3,20 +3,55 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        minLength: 2,
+        maxLength: 30,
     },
     lastName: {
         type: String
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 100,
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+
     },
     password: {
-        type: String
-    }
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 30
+    },
+    about: {
+        type: String,
+        default: "I am a Software Engineer",
+    },
+    skills: {
+        type: [String],
+    },
+    photoUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1BM98aZ18HKEbruHfdxX2GxAaw5MWkSbnvw&s"
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("please select the proper gender");
+            }
+        },
+    },
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('user', userSchema);
