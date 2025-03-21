17b2d5a95fd279e088a143ffd3f6a51921a687d6
reddymanyam <reddappareddy18@gmail.com>
2025-03-21 17:37:13 +0530

    updated: signin and login credentials are added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const connectDB = require('./config/database');
 const app = express();
 const User = require('./model/user');
+const bcrypt = require('bcrypt');
+
+const { validationSignUp } = require('./utils/validation');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT;
@@ -20,8 +24,16 @@ app.get('/users', async (req, res) => {
 app.post('/users', async (req, res) => {
 
     try {
+        const { firstName, lastName, email, password } = req.body;
 
-        const users = new User(req.body);
+        // validations
+        validationSignUp(req);
+
+        // password encryption
+        const hashPassword = await bcrypt.hash(password, 10);
+        // console.log(hashPassword);
+
+        const users = new User({ firstName, lastName, email, password: hashPassword });
         await users.save();
         res.status(200).json(users);
     } catch (err) {
@@ -30,6 +42,50 @@ app.post('/users', async (req, res) => {
 }
 );
 
+app.post('/signup', async (req, res) => {
+
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        // validations
+        validationSignUp(req);
+
+        // password encryption
+        const hashPassword = await bcrypt.hash(password, 10);
+        // console.log(hashPassword);
+
+        const users = new User({ firstName, lastName, email, password: hashPassword });
+        await users.save();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: "something went wrong", error: err.message });
+    }
+}
+);
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+        console.log(user);
+
+        if (!user) {
+            throw new Error("Invalid Credentials")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new Error("Invalid Creditials");
+        } else {
+            res.status(200).json({ message: "User Logged in Successfully" })
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: "something went wrong", error: err.message })
+    }
+})
 
 app.patch('/users/:id', async (req, res) => {
 
