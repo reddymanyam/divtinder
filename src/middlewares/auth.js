b50d5c6c625eba5f163e51241694c4ddb0c32fe9
reddymanyam <reddappareddy18@gmail.com>
2025-03-23 23:44:44 +0530

    updated: auth middleware is updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,34 @@
 const jwt = require('jsonwebtoken');
+const User = require('../model/user');
 
+const adminAuth = async (req, res, next) => {
+    try {
+        // Get token from cookies or authorization header
+        const token = req.cookies.token ||
+            (req.headers.authorization && req.headers.authorization.split(' ')[1]);
 
-const adminAuth = (req, res, next) => {
+        if (!token) {
+            return res.status(401).json({ message: "Access Denied. No Token Provided!" });
+        }
 
-    const cookies = req.cookies;
-    const { token } = cookies;
+        const decodedObj = jwt.verify(token, "Reddy@#123");
 
-    const isAuthenticated = jwt.verify(token, "Reddy@#123");
+        const { _id } = decodedObj;
+        const user = await User.findById(_id);
 
-    if (!isAuthenticated) {
-        res.status(402).send("authentication failed");
-    } else {
-        next()
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        // Return a proper JSON error response
+        if (err.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: "Invalid token", error: err.message });
+        }
+        return res.status(500).json({ message: "Something Went Wrong", error: err.message });
     }
-}
+};
 
 module.exports = { adminAuth };
