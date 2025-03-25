d1879b2ad32245b6bda90138318e7dcc6a9bdac1
reddymanyam <reddappareddy18@gmail.com>
2025-03-25 16:39:15 +0530

    updated: profile view and edit apis are writtten

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const profileRouter = express.Router();
 const { adminAuth } = require('../middlewares/auth');
 
-profileRouter.get('/profile', adminAuth, async (req, res) => {
+profileRouter.get('/profile/view', adminAuth, async (req, res) => {
 
     try {
         const user = req.user;
@@ -12,4 +12,11 @@ profileRouter.get('/profile', adminAuth, async (req, res) => {
     }
 });
 
+profileRouter.patch('/profile/edit', adminAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).send({ message: "Something Went Wrong", err })
+    }
+})
 module.exports = { profileRouter };
