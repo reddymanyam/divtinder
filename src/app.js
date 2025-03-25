7920777ccb88f9735cc7f3ad85eb7788596157e1
reddymanyam <reddappareddy18@gmail.com>
2025-03-25 13:41:44 +0530

    updated: app.js is updated with by modifing all routes

--- src/app.js
+++ src/app.js
@@ -1,170 +1,22 @@
 const express = require("express");
 const connectDB = require('./config/database');
 const app = express();
-const User = require('./model/user');
-const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
 
-const { validationSignUp } = require('./utils/validation');
-const { adminAuth } = require('./middlewares/auth');
 
 require('dotenv').config();
-
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});  // Get the all the data from the database
-        res.status(200).json({ "message": "Data is Successfully send", users })
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", error: err.message });  // This is very Important for knowing what type of error , it is throwing
-    }
-});
+const { authRouter } = require('./routes/auth');
+const { profileRouter } = require('./routes/profile');
+const { requestRouter } = require('./routes/request');
 
-app.post('/users', async (req, res) => {
-
-    try {
-        const { firstName, lastName, email, password } = req.body;
-
-        // validations
-        validationSignUp(req);
-
-        // password encryption
-        const hashPassword = await bcrypt.hash(password, 10);
-        // console.log(hashPassword);
-
-        const users = new User({ firstName, lastName, email, password: hashPassword });
-        await users.save();
-        res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", error: err.message });
-    }
-}
-);
-
-app.post('/signup', async (req, res) => {
-
-    try {
-        const { firstName, lastName, email, password } = req.body;
-
-        // validations
-        validationSignUp(req);
-
-        // password encryption
-        const hashPassword = await bcrypt.hash(password, 10);  // creating a hashpassword (password, saltNumber)
-        // console.log(hashPassword);
-
-        const users = new User({ firstName, lastName, email, password: hashPassword });
-        await users.save();
-        res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", error: err.message });
-    }
-}
-);
-
-app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        // const user_id = user._id;
-
-        console.log("User Found:", user);
-
-        if (!user) {
-            throw new Error("Invalid Credentials");
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        console.log("Password Match:", isPasswordValid);
-
-        if (isPasswordValid) {
-
-            // Generate JWT token
-            const token = await jwt.sign({ _id: user._id }, "Reddy@#123", { expiresIn: '1h' });  //hiddencode and secret number
-            console.log("token is...", token);
-
-            // Set the token in a cookie
-            res.cookie("token", token, { expires: new Date(Date.now() + 900000), httpOnly: true });
-            res.send("Login Succesful!!!");
-        } else {
-            throw new Error("Invalid Credentials")
-        }
-
-    } catch (err) {
-        res.status(500).json({ message: "Something went wrong", error: err.message });
-    }
-});
-
-
-app.get('/profile', adminAuth, async (req, res) => {
-
-    try {
-        const user = req.user;
-        res.send(user);
-    } catch (err) {
-        res.status(500).send({ message: "Something went Wrong", err })
-    }
-});
-
-app.post('/sendConnectionRequest', adminAuth, async (req, res) => {
-    try {
-        user = req.user;
-        const { firstName } = user;
-
-        res.status(201).send(firstName + "sent the connection request")
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", err: err.message });
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-
-    const reqId = req.params.id;
-    const updatedData = req.body;
-
-    try {
-
-        const ALLOWED_UPDATES = ["userId", "firstName", "lastName", "skills", "about"];
-        const isAllowedUpdates = Object.keys(updatedData).every((k) => ALLOWED_UPDATES.includes(k));
-
-        if (!isAllowedUpdates) {
-            throw new Error("update not allowed");
-        }
-
-        if (updatedData.skills.length > 10) {
-            throw new Error("you cannot add more than 10 skills");
-        }
-        const users = await User.findOneAndUpdate({ _id: reqId }, updatedData, {
-            returnDocument: "after",
-            returnValidators: true,
-        });
-        if (!users) {
-            res.status(404).json({ message: "users is not found" });
-        }
-        res.status(200).json({ message: "users Data Updated Succesfully", users });
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", error: err.message });
-    }
-});
-
-app.delete('/users/:id', async (req, res) => {
-
-    const reqId = req.params.id;
-    try {
-        const users = await User.findByIdAndDelete({ _id: reqId });
-        if (!users) {
-            res.status(404).json({ message: "users is not found" });
-        }
-        res.status(200).json({ message: "data deleted succesfully", users });
-    } catch (err) {
-        res.status(500).json({ message: "something went wrong", error: err.message });
-    }
-})
+app.use('/', authRouter);
+app.use('/', profileRouter);
+app.use('/', requestRouter);
 
 connectDB()
     .then(() => {
