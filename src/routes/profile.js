cd9371c80dc9f67507e7184d33a9ec7183467fd3
reddymanyam <reddappareddy18@gmail.com>
2025-04-02 22:56:29 +0530

    updated: profile router is updated

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,21 +2,38 @@ const express = require('express');
 const profileRouter = express.Router();
 const { adminAuth } = require('../middlewares/auth');
 
+// View Profile Route
 profileRouter.get('/profile/view', adminAuth, async (req, res) => {
-
     try {
-        const user = req.user;
-        res.send(user);
+        if (!req.user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+        res.status(200).send(req.user);
     } catch (err) {
-        res.status(500).send({ message: "Something went Wrong", err })
+        res.status(500).send({ message: "Something went wrong", error: err.message });
     }
 });
 
+// Edit Profile Route
 profileRouter.patch('/profile/edit', adminAuth, async (req, res) => {
     try {
+        const user = req.user;
+        const updates = req.body;
+        
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+        
+        Object.keys(updates).forEach(key => {
+            user[key] = updates[key];
+        });
 
+        await user.save(); // Assuming user is a Mongoose model
+
+        res.status(200).send({ message: "Profile updated successfully", user });
     } catch (err) {
-        res.status(500).send({ message: "Something Went Wrong", err })
+        res.status(500).send({ message: "Something went wrong", error: err.message });
     }
-})
+});
+
 module.exports = { profileRouter };
